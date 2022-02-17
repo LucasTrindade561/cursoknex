96a0f8fd73d7412537827452ca808790be538ae0
Lucas Trindade <nh.lucass@gmail.com>
2022-02-17 14:34:44 -0300

    select whre

--- knex/queries/select-column.js
+++ knex/queries/select-column.js
@@ -1,5 +1,18 @@
 /*
 SELECT 
-u.email, u.id uid, u.first_name ufirstname FROM users u;
+u.email uemail, u.id uid, u.first_name ufirstname FROM users u;
 */
 const knex = require('../config/database');
+
+const select = knex('users as u')
+    .select('u.email as uemail', 'u.id as uid', 'u.first_name as ufirstname');
+
+select.then(data => {
+    for(const user of data) {
+        console.log(user);
+    }
+}).catch(e => {
+    console.log('ERROR',e.message);
+}).finally(() => {
+    knex.destroy();
+});
